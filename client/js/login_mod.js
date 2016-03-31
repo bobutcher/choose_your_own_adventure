b8c2a1a68583106191e7453abe99e8ef3d1b6068
MJG <MJG@MacBook-Pro.local>
2016-03-31 15:56:49 -0400

    login module working. Also a few small things.

---
+++ client/js/login_mod.js
@@ -0,0 +1,43 @@
+(function(ns) {
+  'use strict';
+  var token;
+
+/**
+ * This function listens for a form submission and does three things:
+ * 1. It sends an ajax request to the server asking for a token
+ * 2. It stores that token for later use
+ * 3. It removes the login UI and replaces it with the story creation UI
+ * @param  {[type]} '#loginform'
+ */
+  $('#loginform').on('submit', function logIn(event){
+    event.preventDefault();
+    getToken(initStoryUI);
+  });
+
+  function getToken(callback){
+    $.ajax({
+      type: 'GET',
+      url: '/login',
+      datatype: 'JSON',
+      success: function gotToken(data){
+        token = data;
+        callback();
+      },
+      error: function noToken(xhr){
+        console.log('Error on getToken fn: ' + xhr);
+      }
+    });
+  }
+
+  ns.currentToken = function currentToken(){
+    return token;
+  };
+
+  function initStoryUI() {
+    $('#login').css({display: 'none'});
+    $('#mainNav').css({display: 'block'});
+
+  }
+
+  window.ns = ns;
+}(window.ns || {}));
